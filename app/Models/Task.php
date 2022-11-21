bf2d99947ebacd90bf55455a4b7a3073303a42ae
lolmeherti <deampuleadd@yandex.com>
2022-11-21 19:33:50 +0100

    [ADD][MODEL] adds fillable properties to the model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,8 +4,38 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class Task extends Model
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
+
+    public mixed $description;
+    public string $repeating;
+    public string $monday;
+    public string $tuesday;
+    public string $wednesday;
+    public string $thursday;
+    public string $friday;
+    public string $saturday;
+    public string $sunday;
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'description',
+        'repeating',
+        'monday',
+        'tuesday',
+        'wednesday',
+        'thursday',
+        'friday',
+        'saturday',
+        'sunday',
+    ];
 }
