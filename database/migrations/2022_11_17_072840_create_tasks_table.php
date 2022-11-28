0836c0129d86a16c63cf176891283b4f31d23fc5
lolmeherti <deampuleadd@yandex.com>
2022-11-28 10:48:20 +0100

    [CHANGE][MIGRATION] task descriptions are now unique

--- database/migrations/2022_11_17_072840_create_tasks_table.php
+++ database/migrations/2022_11_17_072840_create_tasks_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->string('description');
+            $table->string('description')->unique();
             $table->string('repeating')->nullable()->default('off');
             $table->string('monday')->nullable()->default('off');
             $table->string('tuesday')->nullable()->default('off');
