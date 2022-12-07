7783c501163abc15b9cb4ea15f71e74da8a831c6
lolmeherti <deampuleadd@yandex.com>
2022-12-07 12:15:54 +0100

    [CHANGE][MIGRATION] adds a default value to the completed column in completions table

--- database/migrations/2022_11_25_102017_create_task_completions_table.php
+++ database/migrations/2022_11_25_102017_create_task_completions_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('task_completions', function (Blueprint $table) {
             $table->id();
             $table->string('date');
-            $table->string('completed');
+            $table->string('completed')->default('off');
             $table->foreignId('task_fid');
             $table->foreignId('user_fid');
             $table->timestamps();
