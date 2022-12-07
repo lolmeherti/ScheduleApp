129198e25784d5c19dd06731abc0e15f80b237c3
lolmeherti <deampuleadd@yandex.com>
2022-12-07 04:09:29 +0100

    [CHANGE][ROUTES] index is now /list. dashboard is bypassed, instead /login will show

--- routes/web.php
+++ routes/web.php
@@ -24,18 +24,19 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/list', [TaskController::class, 'index'])->name('list');
-Route::post('/list', [TaskController::class, 'store'])->name('store');
-Route::post('/list/edit', [TaskController::class, 'edit'])->name('edit');
-Route::get('/list/show/{id}', [TaskController::class, 'show'])->name('show');
+Route::group(['middleware' => 'auth'], function () { //these routes are only available to authenticated users!
 
-Route::get('/list/create_completions/{id}', [TaskCompletionController::class, 'completeTaskById'])->name('complete');
-Route::post('/list/delete_completion/', [TaskCompletionController::class, 'deleteTaskCompletionTaskById'])->name('delete_completion');
+    Route::get('/list', [TaskController::class, 'index'])->name('list');
+    Route::post('/list', [TaskController::class, 'store'])->name('store');
+    Route::post('/list/edit', [TaskController::class, 'edit'])->name('edit');
+    Route::get('/list/show/{id}', [TaskController::class, 'show'])->name('show');
 
-Route::middleware('auth')->group(function () {
+    Route::get('/list/create_completions/{id}', [TaskCompletionController::class, 'completeTaskById'])->name('complete');
+    Route::post('/list/delete_completion/', [TaskCompletionController::class, 'deleteTaskCompletionTaskById'])->name('delete_completion');
+
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
